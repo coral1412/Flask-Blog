4e0ff442ffb6bd691c510f1a5f39b4828ca763a1
coral <guangyichan@gmail.com>
2017-01-19 13:25:34 +0800

    add flask-wtf

--- hello.py
+++ hello.py
@@ -1,19 +1,48 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 
 app=Flask(__name__)
 manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
+app.config['SECRET_KEY']="haha"
 
-@app.route('/')
+#@app.route('/')
+#def index():
+##    return '<h1>Hello World</h1>'
+#    return render_template('index.html',current_time=datetime.utcnow())
+
+#@app.route('/',methods=['GET','POST'])
+#def index():
+#    name=None
+#    form=NameForm()
+#    if form.validate_on_submit():
+##        name=form.name.data
+##        form.name.data=''
+##        session['name']=form.name.data
+#        old_name=session.get('name')
+#        if old_name is not None and old_name !=form.name.data:
+#            flash('Looks like you have changed your name!')
+#        session['name']=form.name.data
+#        return redirect(url_for('index'))
+#    return render_template('index.html',form=form,name=session.get('name'))
+@app.route('/',methods=['GET','POST'])
 def index():
-#    return '<h1>Hello World</h1>'
-    return render_template('index.html',current_time=datetime.utcnow())
+    form=NameForm()
+    if form.validate_on_submit():
+        old_name=session.get('name')
+        if old_name is not None and old_name !=form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form,name=session.get('name'))
 
 @app.route('/user/<name>')
 def  user(name):
@@ -27,6 +56,12 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"),500
+
+
+class NameForm(Form):
+
+    name=StringField('what is your name?',validators=[Required()])
+    submit=SubmitField('Submit')
 
 
 if __name__=='__main__':
