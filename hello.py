275efcdda67da4b1e40f22ec3cf88c736e7ac451
coral <guangyichan@gmail.com>
2017-01-19 14:07:11 +0800

    add mysql

--- hello.py
+++ hello.py
@@ -6,33 +6,21 @@ from datetime import datetime
 from flask_wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+basedir=os.path.abspath(os.path.dirname(__file__))
 
 app=Flask(__name__)
 manager=Manager(app)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 app.config['SECRET_KEY']="haha"
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://flasky:flasky@654321@192.168.100.100:3306/flasky'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+app.config['SQLALCHEMY_TRACK_MODIFICANTS']=True
 
-#@app.route('/')
-#def index():
-##    return '<h1>Hello World</h1>'
-#    return render_template('index.html',current_time=datetime.utcnow())
-
-#@app.route('/',methods=['GET','POST'])
-#def index():
-#    name=None
-#    form=NameForm()
-#    if form.validate_on_submit():
-##        name=form.name.data
-##        form.name.data=''
-##        session['name']=form.name.data
-#        old_name=session.get('name')
-#        if old_name is not None and old_name !=form.name.data:
-#            flash('Looks like you have changed your name!')
-#        session['name']=form.name.data
-#        return redirect(url_for('index'))
-#    return render_template('index.html',form=form,name=session.get('name'))
+db=SQLAlchemy(app)
 @app.route('/',methods=['GET','POST'])
 def index():
     form=NameForm()
@@ -58,10 +46,32 @@ def internal_server_error(e):
     return render_template("500.html"),500
 
 
+#flask_wtf
 class NameForm(Form):
 
     name=StringField('what is your name?',validators=[Required()])
     submit=SubmitField('Submit')
+
+
+#databases
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+    users=db.relationship('User',backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(64),unique=True,index=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 if __name__=='__main__':
