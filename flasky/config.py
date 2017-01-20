4486cddfaec53b2c221bc57e763930355be42eaa
coral <guangyichan@gmail.com>
2017-01-20 15:29:46 +0800

    daxing

--- flasky/config.py
+++ flasky/config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN=True
     FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
     FLASKY_MAIL_SENDER='gyc1412@163.com'
-    FLASKY_ADMIN=os.envirion.get('FLASKY_ADMIN)')
+    FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN)')
 
     @staticmethod
     def init_app(app):
@@ -33,7 +33,7 @@ class ProductionConfig(Config):
 
 config={
     'development':DevelopmentConfig,
-    'testing':TestingCofig,
+    'testing':TestingConfig,
     'production':ProductionConfig,
     'default':DevelopmentConfig
 }
